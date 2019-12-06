dfdce82234503a5608318b963f09178cb624fbcc
Álvaro Oliveira <alvarogab6@gmail.com>
2019-12-06 16:19:50 -0300

    update config database

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,19 @@
 const Sequelize = require('sequelize');
 
+const Bucket = require('../api/schemas/bucket.schema');
+const Manager = require('../api/schemas/manager.schema');
+const User = require('../api/schemas/user.schema');
+
 const database = new Sequelize({
     dialect: 'sqlite',
     storage: './data/storage.sqlite',
     logging: false,
 });
+
+Bucket.init(database);
+Manager.init(database);
+User.init(database);
+
+User.associate(database.models);
 
 module.exports = database;
