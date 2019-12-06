1e4b2cfe70cc110a5767cfaec2219bda4a1f35b7
Álvaro Oliveira <alvarogab6@gmail.com>
2019-12-06 16:20:23 -0300

    add config winston

---
+++ src/config/winston.js
@@ -0,0 +1,38 @@
+const winston = require('winston');
+const useragent = require('useragent');
+const moment = require('moment-timezone');
+
+const timestamp = winston.format((info) => {
+    info.timestamp = moment().tz('America/Fortaleza').format();
+    return info;
+});
+
+const agent = winston.format((info) => {
+    const user = useragent.parse(info.agent);
+    info.agent = { agent: user.toAgent(), os: user.os.toString() };
+    return info;
+});
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.File({
+            filename: 'error.log',
+            level: 'error',
+            format: winston.format.combine(
+                timestamp(),
+                agent(),
+                winston.format.json(),
+            ),
+        }),
+        new winston.transports.File({
+            filename: 'logger.log',
+            format: winston.format.combine(
+                timestamp(),
+                agent(),
+                winston.format.json(),
+            ),
+        }),
+    ],
+});
+
+module.exports = logger;
