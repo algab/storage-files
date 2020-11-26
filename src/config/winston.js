49dfe7ddd9d9d8c5adca3386f5e0b540909ba3fd
Álvaro Oliveira <alvarogab6@gmail.com>
2020-11-26 10:21:33 -0300

    update folder config

--- src/config/winston.js
+++ src/config/winston.js
@@ -3,36 +3,23 @@ const useragent = require('useragent');
 const moment = require('moment-timezone');
 
 const timestamp = winston.format((info) => {
-    info.timestamp = moment().tz('America/Fortaleza').format();
-    return info;
+  info.timestamp = moment().tz('America/Fortaleza').format();
+  return info;
 });
 
 const agent = winston.format((info) => {
-    const user = useragent.parse(info.agent);
-    info.agent = { agent: user.toAgent(), os: user.os.toString() };
-    return info;
+  const user = useragent.parse(info.agent);
+  info.agent = { agent: user.toAgent(), os: user.os.toString() };
+  return info;
 });
 
 const logger = winston.createLogger({
-    transports: [
-        new winston.transports.File({
-            filename: 'error.log',
-            level: 'error',
-            format: winston.format.combine(
-                timestamp(),
-                agent(),
-                winston.format.json(),
-            ),
-        }),
-        new winston.transports.File({
-            filename: 'logger.log',
-            format: winston.format.combine(
-                timestamp(),
-                agent(),
-                winston.format.json(),
-            ),
-        }),
-    ],
+  transports: [
+    new winston.transports.File({
+      filename: 'logger.log',
+      format: winston.format.combine(timestamp(), agent(), winston.format.json()),
+    }),
+  ],
 });
 
 module.exports = logger;
